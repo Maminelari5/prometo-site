23efecd169549fcf11e5edbed1772afd1a04756a
Amine <aminelari25@gmail.com>
2026-04-10 01:47:35 +0100

    add migrate route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\Api\ProjectController;
 use App\Http\Controllers\Api\PublicProjectController;
 use App\Http\Controllers\Api\ContactController;
 use Illuminate\Support\Facades\Artisan;
-
+Route::get('/test-api', function () {
+    return response()->json(['ok' => true]);
+});
 Route::get('/migrate', function () {
     Artisan::call('migrate', ['--force' => true]);
     return 'migrated';
